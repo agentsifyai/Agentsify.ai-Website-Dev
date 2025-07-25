7df27fc0095db8b7d2b9be5ab25ad8fc0f7e4416
Vaishnavi Mantri <155553314+VaishnaviMantri09@users.noreply.github.com>
2025-07-25 01:29:08 -0400

    Updated Contact Us index.jsx

--- pages/contactus/index.jsx
+++ pages/contactus/index.jsx
@@ -73,7 +73,7 @@ const ContactUs = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('/', {
+      const response = await fetch('/__forms.html', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: encode({
